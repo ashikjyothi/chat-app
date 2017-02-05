cbcfb54f375d0da41e30214600ee9dabb59393ef
Ashik Jyothi <ashikjyothi007@gmail.com>
2017-02-05 19:24:02 +0530

    Private chat added

--- public/app.js
+++ public/app.js
@@ -1,150 +1,174 @@
-angular.module('myApp',['ui.router'])
-    .service('Session', function(){
-        this.user = null;
-        this.setUser = function(Ud,cb){
-            console.log("Service setuser start")
-            this.user = Ud.username;
-            this.room = Ud.room;
-            cb();
-
+angular.module('myApp', ['ui.router']).service('Session', function() {
+    this.user = localStorage['user'] ? localStorage['user'] : null;
+    this.room = localStorage['room'] ? localStorage['room'] : null;
+    this.setUser = (Ud, cb) => {
+        this.user = Ud.username;
+        this.room = Ud.room;
+        localStorage['user'] = Ud.username;
+        localStorage['room'] = Ud.room;
+        console.log('Setting userdata')
+        cb();
+    }
+    this.clearUser = (cb) => {
+        this.user = '';
+        this.room = ''
+        delete localStorage['user'];
+        delete localStorage['room'];
+        cb();
+    }
+}).config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider.state('main', {
+        url: '/'
+    }).state('login', {
+        url: '/login',
+        views: {
+            'content': {
+                templateUrl: 'login.html'
+            }
+        }
+    }).state('chat', {
+        url: '/chat',
+        views: {
+            'content': {
+                templateUrl: 'chat.html'
+            }
         }
     })
-    .config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
-        $urlRouterProvider.otherwise('/');
-        $stateProvider
-            .state('main',{
-                url : '/'
-            })
-            .state('login',{
-                url : '/login',
-                views: {
-                    'content': {
-                        templateUrl : 'login.html'
-                    }
-                }
-            })
-            .state('chat',{
-                url : '/chat',
-                views: {
-                    'content': {
-                        templateUrl : 'chat.html'
-                    }
-                }
-            })
-
-    }])
-    .run(function($rootScope,$state,Session){
-        $rootScope.$on('$stateChangeStart', function(e, toState) {
-			if (toState.url == '/') {
-				if (Session.user) {
-					e.preventDefault();
-					$state.go('chat', {});
-				} else {
-					e.preventDefault();
-					$state.go('login', {});
-				}
-			} else if (toState.url == '/chat') {
-				if (!Session.user) {
-					e.preventDefault();
-					$state.go('login', {});
-				}
-			} else if (toState.url == '/login') {
-				if (Session.user) {
-					e.preventDefault();
-					$state.go('chat', {});
-				}
-			}
-		})
-    })
-    .service('Socket',function($timeout){
-        var socket = io();
-
-        this.emit = function(event,data,cb){
-            console.log("Emitting::",event,data);
-            socket.emit(event,data,function(response){
-                if(cb){
-                    $timeout(function(){
-                        cb(response);
-                    })
-                    
-                }
-            });
+}]).run(function($rootScope, $state, Session) {
+    $rootScope.$on('$stateChangeStart', function(e, toState) {
+        if (toState.url == '/') {
+            if (Session.user) {
+                e.preventDefault();
+                $state.go('chat', {});
+            } else {
+                e.preventDefault();
+                $state.go('login', {});
+            }
+        } else if (toState.url == '/chat') {
+            if (!Session.user) {
+                e.preventDefault();
+                $state.go('login', {});
+            }
+        } else if (toState.url == '/login') {
+            if (Session.user) {
+                console.log('HEREEEEEEEEEEEEEE')
+                e.preventDefault();
+                $state.go('chat', {});
+            }
         }
-
-        this.on = function(event,callback){
-            socket.on(event,function(cb){
-                $timeout(function(){
-                    callback(cb);
+    })
+}).service('Socket', function($timeout, Session) {
+    var socket = io();
+    socket.on('connect', function() {
+        if (Session.user) {
+            socket.emit('initSocket', Session.user)
+        }
+    })
+    this.emit = function(event, data, cb) {
+        console.log("Emitting::", event, data);
+        socket.emit(event, data, function(response) {
+            if (cb) {
+                $timeout(function() {
+                    cb(response);
                 })
+            }
+        });
+    }
+    this.on = function(event, callback) {
+        socket.on(event, function(cb) {
+            $timeout(function() {
+                callback(cb);
             })
-        }
-    })
-    .controller('loginController',['$scope','$location','Session','Socket',function($scope,$location,Session,Socket){
-        console.log("Socket::",Socket);
-        $scope.errorText = "";
-        $scope.login = function(){
-            var usrDetails = {username: $scope.username, room: $scope.room};
-            // console.log("usrDetails:"+ usrDetails.username+" "+ usrDetails.room);
-
-        Socket.emit('joinRoom',{
+        })
+    }
+}).controller('loginController', ['$scope', '$location', 'Session', 'Socket', function($scope, $location, Session, Socket) {
+    console.log("Socket::", Socket);
+    $scope.errorText = "";
+    $scope.login = function() {
+        var usrDetails = {
+            username: $scope.username,
+            room: $scope.room
+        };
+        // console.log("usrDetails:"+ usrDetails.username+" "+ usrDetails.room);
+        Socket.emit('joinRoom', {
             name: usrDetails.username,
             room: usrDetails.room
         })
         // Socket.on('clientInfo',function(ci){
         //     console.log("clientInfo"+ci);
         // })
-
-            
-
-
-            Socket.emit('register',usrDetails,function(response){
-                    console.log('login Response',response)
-                    if(response == 'success'){
-                        Session.setUser(usrDetails,function(response){
-                            $location.path('/');
-                        })
-                    }
-                    if(response == 'error'){
-                        $scope.errorText = "Username already taken";
-                    }
+        Socket.emit('register', usrDetails, function(response) {
+            console.log('login Response', response)
+            if (response == 'success') {
+                Session.setUser(usrDetails, function(response) {
+                    $location.path('/');
+                })
+            }
+            if (response == 'error') {
+                $scope.errorText = "Username already taken";
+            }
+        });
+    }
+}]).controller('chatController', ['$scope', 'Socket', 'Session', '$state', '$timeout', function($scope, Socket, Session, $state, $timeout) {
+    $scope.user = Session.user;
+    $scope.room = Session.room;
+    $scope.disconnect = function() {
+        Socket.emit('logout', $scope.user, function() {
+            console.log('Logout success')
+            Session.clearUser(function() {
+                $state.go('login', {});
+            })
+        })
+    }
+    $scope.sendMessage = function(text) {
+        if (text && text.substr(0, 1) === '/') {
+            var privatemsg = {};
+            privatemsg.user = text.substr(1, text.indexOf(' '));
+            privatemsg.user = privatemsg.user.trim();
+            privatemsg.msg = text.substring(text.indexOf(' '));
+            privatemsg.room = $scope.room;
+            Socket.emit("PrivateMsg", privatemsg, function(response) {
+                console.log("PrivateMsg response:" + response);
             });
-        }
-    }])
-    .controller('chatController',['$scope','Socket','Session',function($scope,Socket,Session){
-        $scope.user = Session.user;
-        $scope.room = Session.room;
-        $scope.sendMessage = function (text){
-            console.log("sendMessage function start");
-            if(text){
-                var timestamp = moment().valueOf();
-                var momentTime = moment.utc(timestamp);
-                    momentTime = momentTime.local().format('h:mm a');
-
-                var newMessage = {
-                    sender : $scope.user,
-                    text : text,
-                    time : momentTime,
-                    room : $scope.room
-                }
-            Socket.emit("chatMessage",newMessage,function(response){
-                if(response == 'success'){
+        } else {
+            var timestamp = moment().valueOf();
+            var momentTime = moment.utc(timestamp);
+            momentTime = momentTime.local().format('h:mm a');
+            var newMessage = {
+                sender: $scope.user,
+                text: text,
+                time: momentTime,
+                room: $scope.room
+            }
+            Socket.emit("chatMessage", newMessage, function(response) {
+                if (response == 'success') {
                     $scope.messages.push(newMessage)
                     $scope.messageInput = "";
+                    $timeout(() => {
+                        var container = document.getElementById('messageContainer');
+                        container.scrollTop = container.scrollHeight - container.clientHeight;
+                    });
                 }
             });
-            }
         }
-
-        $scope.getMessages = function(){
-        Socket.emit('getMessages',{},function(messages){
-            console.log('Messages:',messages)
+    }
+    $scope.getMessages = function() {
+        Socket.emit('getMessages', {}, function(messages) {
+            console.log('Messages:', messages)
             $scope.messages = messages;
+            $timeout(() => {
+                var container = document.getElementById('messageContainer');
+                container.scrollTop = container.scrollHeight - container.clientHeight;
+            });
         })
-        }
-     $scope.getMessages();
-
-     Socket.on('chatMessage',function(message){
-         $scope.messages.push(message);
-     })
-
-    }])
+    }
+    $scope.getMessages();
+    Socket.on('chatMessage', function(message) {
+        $scope.messages.push(message);
+        $timeout(() => {
+            var container = document.getElementById('messageContainer');
+            container.scrollTop = container.scrollHeight - container.clientHeight;
+        });
+    })
+}])
--- server.js
+++ server.js
@@ -1,163 +1,221 @@
 var express = require('express'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    MongoStore = require('connect-mongo')(session),
     mongoose = require('mongoose'),
     socketio = require('socket.io'),
     moment = require('moment'),
     http = require('http');
- var app = express();
 
- var server = http.createServer(app).listen(3000);
+var app = express();
 
- var io = socketio.listen(server);
+var server = http.createServer(app).listen(3000);
+var io = socketio.listen(server);
 
- app.use(express.static(__dirname + '/public'));
+// app.use(cookieParser);
 
- var clientInfo = {};
+// app.use(session({
+//     secret: 'mysecret',
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {
+//         secure: true
+//     },
+//     store: new MongoStore({
+//         url: 'mongodb://localhost:27017/chatapp'
+//     })
+// }))
 
- mongoose.connect("mongodb://localhost:27017/chatapp", function(err){
-     if(err)
-     {
-         console.log("Error connecting to database");
-     }else{
-         console.log("Mongo Working");
-     }
- })
+app.use(express.static(__dirname + '/public'));
+var clientInfo = {};
+mongoose.connect("mongodb://localhost:27017/chatapp", function(err) {
+    if (err) {
+        console.log("Error connecting to database");
+    } else {
+        console.log("Mongo Working");
+    }
+})
+var userSchema = mongoose.Schema({
+    socketid: String,
+    username: {
+        type: String,
+        unique: true
+    },
+    room: String
+});
+var User = mongoose.model('User', userSchema);
+var chatSchema = mongoose.Schema({
+    sender: String,
+    text: String,
+    time: String,
+    room: String
+});
+var Chat = mongoose.model('Chat', chatSchema);
 
- var userSchema = mongoose.Schema({
-    socketid : String,
-     username : {
-         type : String,
-         unique : true
-     },
-     room : String
- }); 
+function addUser(un, cb) {
+    var newUser = new User({
+        socketid: un.socketid,
+        username: un.username,
+        room: un.room
+    })
+    newUser.save(function(error, result) {
+        if (error) {
+            console.log("Error Saving Data", error)
+            cb("error");
+        } else {
+            console.log("Username saved");
+            cb("success");
+        }
+    })
+}
 
- var User = mongoose.model('User', userSchema);
-
- var chatSchema = mongoose.Schema({
-     sender : String,
-     text : String,
-     time : String,
-     room : String
- });
-
- var Chat =  mongoose.model('Chat', chatSchema);
-
- function addUser(un,cb){
-     var newUser = new User({
-         username : un
-     })
-     newUser.save(function(error, result){
-         if(error){
-             console.log("Error Saving Data",error)
-             cb("error");
-         }else {
-             console.log("Username saved");
-             cb("success");
-         }
-     })
- }
-
- function addMessage(message,cb){
-     
-
-     var newMsg = new Chat({
-         sender : message.sender,
-         text : message.text,
-         time : message.time,
-         room : message.room
-     })
-     newMsg.save(function(error,result){
-         if(error){
-             console.log("Error:",error);
-         }else{
-             console.log("Database messsage save:",result);
-             cb('success');
-         }
-     })
- }
-
-io.on('connection',function(socket){
-
+function addMessage(message, cb) {
+    var newMsg = new Chat({
+        sender: message.sender,
+        text: message.text,
+        time: message.time,
+        room: message.room
+    })
+    newMsg.save(function(error, result) {
+        if (error) {
+            console.log("Error:", error);
+        } else {
+            // console.log("Database messsage save:",result);
+            cb('success');
+        }
+    })
+}
+io.on('connection', function(socket) {
     var socketID = socket.id;
+    socket.on('initSocket',function(user){
+            console.log("NEW SOCKET ID::"+socket.id);
 
-    socket.on('joinRoom',function(req){
-        clientInfo.socketID= {name:req.name,room:req.room};
- 
+
+           User.findOne({username: user}, function (err, user) {
+                user.socketid = socket.id;
+
+                user.save(function (err) {
+                if(err) {
+                console.error('ERROR!');
+                 }
+                });
+                });
+
+        })
+
+    socket.on('joinRoom', function(req) {
+        clientInfo.socketID = {
+            name: req.name,
+            room: req.room
+        };
         socket.join(req.room);
-        socket.broadcast.to(req.room).emit('chatMessage',{
+        socket.broadcast.to(req.room).emit('chatMessage', {
             sender: "System",
             text: req.name + " has joined chat!",
             time: "",
             room: req.room
         })
     })
-
     console.log("Connected");
-    socket.on('register',function(Ud,fn){
+    socket.on('register', function(Ud, fn) {
+        Ud.socketid = socketID;
+        // console.log("Ud.socketID:"+Ud.socketid);
         console.log("received register request")
-        addUser(Ud.username,function(response){
+        addUser(Ud, function(response) {
             fn(response);
         })
     })
-    socket.on('chatMessage',function(message,fn){
-        addMessage(message,function(response){
-            if(response == 'success'){
-                socket.broadcast.emit('chatMessage',message)
+    socket.on('PrivateMsg', function(pm, fn) {
+        console.log("Received emitted privatemsg from:" + pm.user);
+        User.find({
+            username: pm.user
+        }, function(err, result) {
+            if (err) {
+                console.log("Private message not send-Invalid User");
+            } else {
+                console.log("Array length" + result.length);
+                console.log("Private User name::" + result[0].username);
+                var socketID = result[0].socketid;
+                console.log("Private User SocketID::" + result[0].socketid);
+                io.to(socketID).emit('chatMessage', {
+                    sender: pm.user,
+                    text: pm.msg,
+                    time: "",
+                    room: pm.room
+                })
+                fn(result);
+            }
+        })
+    })
+    socket.on('chatMessage', function(message, fn) {
+        addMessage(message, function(response) {
+            if (response == 'success') {
+                socket.broadcast.emit('chatMessage', message)
                 fn('success');
-            }else {
+            } else {
                 fn('error');
             }
         })
     })
-    socket.on('getMessages',function(input,fn){
-        Chat.find({},function(error,result){
-            if(error){
-                console.log("error:",error);
+    socket.on('getMessages', function(input, fn) {
+        Chat.find({}, function(error, result) {
+            if (error) {
+                console.log("error:", error);
                 fn(error);
-            }else{
+            } else {
                 //console.log("getMessages result:",result);
                 fn(result);
             }
         })
     })
-    socket.on('disconnect',function(){
+    socket.on('logout', function(user, cb) {
 
-        try {
-            var ci = clientInfo.socketID.name;
-            if (ci == 'undefined') throw ('undefined');
-            else {
-
-            socket.broadcast.emit('chatMessage',{
-            sender: "System",
-            text: clientInfo.socketID.name + " has disconnected!",
-            time: "",
-            room: clientInfo.socketID.room
-        })
-            delete clientInfo.socketID;
-
-        }
-            }
-            
-
-        catch (error){
-            console.log(error);
+        if(socket.id){
+            User.remove({
+                socketid: socket.id
+            },function(err, res) {
+                if (err) {
+                    console.log('User remove err:::', err)
+                } else {
+                    cb();
+                }
+            });
         }
 
+
+
+
+        console.log('disconnect')
+        // try {
+        //     var ci = clientInfo.socketID.name;
+        //     if (ci == 'undefined') throw ('undefined');
+        //     else {
+        //         User.remove({
+        //             username: clientInfo.socketID.name
+        //         }, function(err) {
+        //             if (!err) {
+        //                 console.log("Username deleted from database");
+        //             } else {
+        //                 console.log("Error deleting username from database");
+        //             }
+        //         });
+        //         socket.broadcast.emit('chatMessage', {
+        //             sender: "System",
+        //             text: clientInfo.socketID.name + " has disconnected!",
+        //             time: "",
+        //             room: clientInfo.socketID.room
+        //         })
+        //         delete clientInfo.socketID;
+        //     }
+        // } catch (error) {
+        //     console.log(error);
         // }
-        
-        
+        // }
     })
-    
 })
-
-app.get('/', function(req,res){
-     res.sendFile('index.html',{
-         root: path.join(__dirname, '/public')
-     })
-     
-
- })
-
-
-
- console.log("Server listening on port 3000");
+app.get('/', function(req, res) {
+    res.sendFile('index.html', {
+        root: path.join(__dirname, '/public')
+    })
+})
+console.log("Server listening on port 3000");
